5c0cce87a977f419b793bfb30e50193c6c272635
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-03-16 09:31:29 +0530

    added ExampleAverageMetric.java

---
+++ src/metrics/average/ExampleAverageMetric.java
@@ -0,0 +1,30 @@
+package metrics.average;
+
+/**
+ * One object of this Metric object exists in the application, which is a single process which we
+ * would like to collect metrics of. Therefore, same object will be shared by different threads.
+ * Operations on long and double are not atomic.
+ * computeCurrentAverageWithSample method has multiple lines of code, and hence it is easy to see
+ * it is not an atomic operation without the synchronized keyword, moreover, the method does operations
+ * on sampleCount and average, operations on which are non-atomic by default without the volatile
+ * keyword.
+ * We have made only average as atomic because operations (both read and write) are non-atomic
+ * on double are non-atomic by default, and we have a getter defined for it.
+ * Since all operations on sampleCount happen inside the synchronized computeCurrentAverageWithSample
+ * method, we need not make it volatile at this point.
+ */
+public class ExampleAverageMetric {
+
+  private long sampleCount;
+  private volatile double average;
+
+  public double getAverage() {
+    return average;
+  }
+
+  public synchronized void computeCurrentAverageWithSample(long sample) {
+    double currentSum = sampleCount * average;
+    sampleCount++;
+    average = (currentSum + sample) / sampleCount;
+  }
+}
